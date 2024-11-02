261dd07b7093abc2df2cdae0cc34f2edea1e77f4
Aadarsh Suresh <94780937+AadarshS9@users.noreply.github.com>
2024-11-02 14:52:49 +0530

    Create Exp6Part1.m

---
+++ Experiment06/Exp6Part1.m
@@ -0,0 +1,8 @@
+% Text Manipulation
+
+string = input('Please enter a string: ', 's');
+upperstring = upper(string);
+lowerstring = lower(string);
+fprintf('Original String: %s\n', string);
+fprintf('Uppercase String: %s\n', upperstring);
+fprintf('Lowercase String: %s\n', lowerstring);
