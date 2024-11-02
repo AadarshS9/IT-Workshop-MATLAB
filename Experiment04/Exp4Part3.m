e68395383d6f53f5ac75de0a665a73779f821ddd
Aadarsh Suresh <94780937+AadarshS9@users.noreply.github.com>
2024-11-02 14:31:48 +0530

    Create Exp4Part3.m

---
+++ Experiment04/Exp4Part3.m
@@ -0,0 +1,19 @@
+% SWITCH PROGRAM
+choice = input('Kindly enter number of your choice:   1. Area   2. Perimeter');
+radius = input('Kindly enter the radius of the circle: ');
+unit = input('Kindly enter the unit measurement: ','s');
+
+if radius <= 0
+    disp('Circle must have a radius greater than 0')
+else
+    switch(choice)
+        case 1
+            area = pi*(radius^2);
+            fprintf('The area of a circle with radius %.2f%s is: %.2f%s^2\n',radius, unit, area,unit);
+        case 2
+            perimeter = 2*pi*radius;
+            fprintf('The perimeter of a circle with radius %.2f%s is %.2f%s\n', radius, unit, perimeter,unit);
+        otherwise
+            disp('Invalid measurements\n')
+    end
+end
