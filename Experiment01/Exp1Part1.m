9af73b23f4c1792bf86831ff9d8e56ecad2ab068
Aadarsh Suresh <94780937+AadarshS9@users.noreply.github.com>
2024-11-02 14:20:08 +0530

    Create Exp1Part1.m

---
+++ Experiment01/Exp1Part1.m
@@ -0,0 +1,58 @@
+a = 5;
+b = 3;
+sum = a + b
+
+sum =
+
+     8
+
+difference = a - b
+
+difference =
+
+     2
+
+product = a * b
+
+product =
+
+    15
+
+division = a / b
+
+division =
+
+    1.6667
+a == b
+
+ans =
+
+  <a href="matlab:helpPopup('logical')" style="font-weight:bold">logical</a>
+
+   0
+
+a > b
+
+ans =
+
+  <a href="matlab:helpPopup('logical')" style="font-weight:bold">logical</a>
+
+   1
+
+a < b
+
+ans =
+
+  <a href="matlab:helpPopup('logical')" style="font-weight:bold">logical</a>
+
+   0
+
+a >= b
+
+ans =
+
+  <a href="matlab:helpPopup('logical')" style="font-weight:bold">logical</a>
+
+   1
+
+diary off
