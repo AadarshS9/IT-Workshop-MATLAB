fa1eee37f894e6969690720c2bc0d94fe8212097
Aadarsh Suresh <94780937+AadarshS9@users.noreply.github.com>
2024-11-02 14:27:27 +0530

    Create Exp3Part2.m

---
+++ Experiment03/Exp3Part2.m
@@ -0,0 +1,25 @@
+disp('Enter the elements of a 2x2 matrix:');
+Enter the elements of a 2x2 matrix:
+a11 = input('Enter element a11: ');
+Enter element a11: 1
+a12 = input('Enter element a12: ');
+Enter element a12: 2
+a21 = input('Enter element a21: ');
+Enter element a21: 3
+a22 = input('Enter element a22: ');
+Enter element a22: 4
+matrix = [a11, a12; a21, a22]
+
+matrix =
+
+     1     2
+     3     4
+
+transposeMatrix = matrix'
+
+transposeMatrix =
+
+     1     3
+     2     4
+
+diary off
