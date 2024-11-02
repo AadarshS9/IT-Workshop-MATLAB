419045868c0ebc90f3c77edda8e6736db722e23a
Aadarsh Suresh <94780937+AadarshS9@users.noreply.github.com>
2024-11-02 15:11:08 +0530

    Create Exp8Part2.m

---
+++ Experiment08/Exp8Part2.m
@@ -0,0 +1,24 @@
+x=linspace(-5,5,50);
+y=linspace(-5,5,50);
+
+[X,Y]=meshgrid(x,y); 
+z=sin(sqrt(X.^2 + Y.^2)); 
+surf(X,Y,z) 
+colorbar 
+title('3D Surface Plot'); 
+xlabel('x Axis'); 
+ylabel('y Axis');
+zlabel('z Axis'); 
+
+figure 
+plot3(X,Y,z);
+title('3D line Plot');
+xlabel('x Axis');
+ylabel('y Axis');
+zlabel('z Axis'); 
+figure 
+scatter3(X,Y,z);
+title('3D scatter Plot');
+xlabel('x Axis');
+ylabel('y Axis');
+zlabel('z Axis');
